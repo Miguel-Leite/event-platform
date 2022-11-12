9ae5e70346c8538ce9974e73bfe0427465ad301f
Miguel-Leite <miguelleite200leite@email.com>
2022-11-12 13:52:12 +0100

    :sparkles: control router created

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,11 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom/client';
 
-import { ApolloProvider } from '@apollo/client/react';
-
-import App from './App'
-import { client } from './lib/apollo';
+import App from './App';
 import './styles/global.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <ApolloProvider client={client}>
-      <App />
-    </ApolloProvider>
+    <App />
   </React.StrictMode>
 )
